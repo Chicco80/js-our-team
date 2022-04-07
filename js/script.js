700d11bb16d378ca7aa5e839f0f5063888a1c8a9
Chicco80 <chicco80lisa@gmail.com>
2022-04-07 17:03:51 +0200

    literal template

--- js/script.js
+++ js/script.js
@@ -1,45 +1,65 @@
 let team = [
 {
-    img :"img/wayne-barnett-founder-ceo.jpg",
-    name : "Weyne Barnett",
+    img :"../img/wayne-barnett-founder-ceo.jpg",
+    fullname : "Weyne Barnett",
     role : "Founder & CEO"
 
 },
 {
-    img :"img/angela-caroll-chief-editor.jpg",
-    name : "Angela Carrol",
+    img :"../img/angela-caroll-chief-editor.jpg",
+    fullname : "Angela Carrol",
     role : "Chief Editor"
 
 },
 {
-    img :"img/walter-gordon-office-manager.jpg",
-    name : "Walter Gordon",
+    img :"../img/walter-gordon-office-manager.jpg",
+    fullfullname : "Walter Gordon",
     role : "Office Manager"
 
 },
 {
-    img :"img/angela-lopez-social-media-manager.jpg",
-    name : "Angela Lopez",
+    img :"../img/angela-lopez-social-media-manager.jpg",
+    fullname : "Angela Lopez",
     role : "Social Media Manager"
 
 },
 {
-    img :"img/scott-estrada-developer.jpg",
-    name : "Scott Estrada",
+    img :"../img/scott-estrada-developer.jpg",
+    fullname : "Scott Estrada",
     role : "Developer"
 
 },
 {
-    img :"img/barbara-ramos-graphic-designer.jpg",
-    name : "Barbara Ramos",
+    img :"../img/barbara-ramos-graphic-designer.jpg",
+    fullname : "Barbara Ramos",
     role : "Graphic Desiner"
 
 }
 
 ];
-console.log(team)
+// console.log(team)
+
+let template ="";
 
 for(let i = 0; i < team.length; i++){
-    console.log(team[i])
+    // console.log(team[i].img)
+    // console.log(team[i].role)
+    for (let key in team[i++]){
+    // console.log(key);
+    // console.log(team[i][key])
+    template = `
+    <div class="team-card">
+      <div class="card-image">
+       <img src="${team[i].img}"/>
+    </div>
+     <div class="card-text">
+      <h3>${team[i].fullname}</h3>
+      <p>${team[i].role}</p>
+     </div>
+    </div> 
+      `
+      
+    }
 }
-
+template = document.querySelector(".team-container").innerHTML += template;
+console.log("leggi",template)
