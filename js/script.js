206f8373ba11b48456355a62d6fe9b07db500373
Chicco80 <chicco80lisa@gmail.com>
2022-04-07 14:48:49 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let team = [
+{
+    img :"img/wayne-barnett-founder-ceo.jpg",
+    name : "Weyne Barnett",
+    role : "Founder & CEO"
+
+},
+{
+    img :"img/angela-caroll-chief-editor.jpg",
+    name : "Angela Carrol",
+    role : "Chief Editor"
+
+},
+{
+    img :"img/walter-gordon-office-manager.jpg",
+    name : "Walter Gordon",
+    role : "Office Manager"
+
+},
+{
+    img :"img/angela-lopez-social-media-manager.jpg",
+    name : "Angela Lopez",
+    role : "Social Media Manager"
+
+},
+{
+    img :"img/scott-estrada-developer.jpg",
+    name : "Scott Estrada",
+    role : "Developer"
+
+},
+{
+    img :"img/barbara-ramos-graphic-designer.jpg",
+    name : "Barbara Ramos",
+    role : "Graphic Desiner"
+
+}
+
+];
+console.log(team)
+
+for(let i = 0; i < team.length; i++){
+    console.log(team[i])
+}
+
